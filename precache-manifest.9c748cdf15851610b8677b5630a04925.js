f9335aab1b8d1d9402f0f4108093f8562f71a604
linardsblk <linards.bulks@gmail.com>
2019-10-24 00:09:47 +0300

    Updates

---
+++ precache-manifest.9c748cdf15851610b8677b5630a04925.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b88830138b09a926b26d796932601009",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "72b19d73a2592c4c1250",
+    "url": "/drum-machine/static/css/main.a74e50c5.chunk.css"
+  },
+  {
+    "revision": "42563ee827d44423acf0",
+    "url": "/drum-machine/static/js/2.838c1fbb.chunk.js"
+  },
+  {
+    "revision": "72b19d73a2592c4c1250",
+    "url": "/drum-machine/static/js/main.6960e98b.chunk.js"
+  },
+  {
+    "revision": "d65b5d28420d8e25f5f8",
+    "url": "/drum-machine/static/js/runtime-main.38fdf893.js"
+  }
+]);
